5f6e683d066131e5a4d1bba13143825b3e88f379
Pedro Aires <1212436@sga.pucminas.br>
2022-01-13 20:25:35 -0300

    Correções de bugs em Pagamento e CategoriaService

--- src/main/java/br/pedro/cursomc/services/CategoriaService.java
+++ src/main/java/br/pedro/cursomc/services/CategoriaService.java
@@ -41,8 +41,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj,obj);
+		return repo.save(newObj);
 	}
 	public void delete(Integer id) {
 		find(id);
@@ -66,5 +67,11 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDTO) {
 		return new Categoria(objDTO.getId(),objDTO.getNome());
+	}
+	
+	private void updateData(Categoria newObj,Categoria obj) {
+		
+		newObj.setNome(obj.getNome());
+		
 	}
 }
