cd49be4941f2595607cc68ccda08df84bfb12c82
Pedro Aires <1212436@sga.pucminas.br>
2022-01-13 15:23:04 -0300

    CRUD Categoria

--- src/main/java/br/pedro/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/pedro/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package br.pedro.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.pedro.cursomc.domain.Categoria;
 import br.pedro.cursomc.services.CategoriaService;
@@ -19,10 +23,36 @@ public class CategoriaResource {
 	
 	
 	@RequestMapping(value="/{id}",method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id){
+	public ResponseEntity<Categoria> find(@PathVariable Integer id){
 		
 		// Response entity é um objeto complexo com várias informações do protocolo http. Encapsula informações da resposta http para o rest service	
-		Categoria obj = service.buscar(id);
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST) //@RequestBody converte o json para um obj java
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		//recebe a uri do novo recurso inserido
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		//gerar resposta 201 (created)
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value ="{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+		
+	}
+	
+	@RequestMapping(value="/{id}",method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/br/pedro/cursomc/services/CategoriaService.java
+++ src/main/java/br/pedro/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package br.pedro.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.pedro.cursomc.domain.Categoria;
 import br.pedro.cursomc.repositories.CategoriaRepository;
+import br.pedro.cursomc.services.exception.DataIntegrityException;
 import br.pedro.cursomc.services.exception.ObjectNotFoundException;
 
 // Camada de serviços
@@ -21,11 +23,28 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	 
-	public Categoria buscar(Integer id){
+	public Categoria find(Integer id){
 		Optional<Categoria> obj = repo.findById(id);
 		
 			return obj.orElseThrow(()-> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + " Tipo: " + Categoria.class.getName()));
 	}	
-	 
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	public void delete(Integer id) {
+		find(id);
+		try{
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos!");
+		}
+	}
 }
